51e43dab9ad6e9a4f214a3c610c0058fbd5a0648
Hava <94910892+Avv18@users.noreply.github.com>
2022-05-29 01:39:07 +0200

    Add files via upload

---
+++ generator pasw c++.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+int main()
+{
+        srand(time(0));
+        string pass;
+        string data="abcdefghijklmnopqrstuvwxyz"
+                                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                "0987654321"
+                                "~!@#$%^&*()_+={}|[]:>?<;.,";
+        for (int i = 1; i <= 12; i++)
+        {
+
+                 pass=pass+data[rand()%93];
+        }
+        cout <<"Your password:"<< pass;
+return 0;
+}
